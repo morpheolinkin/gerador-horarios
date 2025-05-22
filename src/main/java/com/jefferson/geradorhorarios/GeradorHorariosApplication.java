0923da05ead296350e9379793cd55b3e37e51e5c
Jefferson M. Silva <jeffersonmedeirosdasilva@gmail.com>
2025-05-22 16:43:47 -0300

    Add initial Spring Boot project setup with PostgreSQL
    This commit initializes the project structure with a Spring Boot application, Maven build configuration, and PostgreSQL setup using Docker Compose. It also includes necessary dependencies, application properties, git configurations, and test scaffolding.

---
+++ src/main/java/com/jefferson/geradorhorarios/GeradorHorariosApplication.java
@@ -0,0 +1,13 @@
+package com.jefferson.geradorhorarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorHorariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeradorHorariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jefferson/geradorhorarios/GeradorHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jefferson.geradorhorarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorHorariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
